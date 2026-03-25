98c7d80de9413f1d22bfe453a300a743a967edf2
Ija Maharyta <ijamaharyta@gmail.com>
2026-03-25 17:10:29 +0200

    feat: add initial structure

---
+++ frontend/src/app.js
@@ -0,0 +1,11 @@
+// const cors = require("cors");
+// const express = require("express");
+// const app = express();
+//
+// app.use(cors()); // Allow all origins (for development)
+//
+// const PORT = process.env.PORT || 3000;
+//
+// app.get("/", (req, res) => res.send("OK"));
+//
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ frontend/src/main.js
@@ -0,0 +1,14 @@
+// import { Routes, Route } from "react-router-dom";
+// import './main.css'
+// import pages
+//
+// function Main(){
+//     return (
+//         <Routes>
+//             <Route path="/" element = {sessionStorage.getItem("token") ? <DashboardPage/> : <LandingPage />}/>
+//             <Route path="/..." element={some page} />
+//         </Routes>
+//     );
+// }
+//
+// export default Main;
