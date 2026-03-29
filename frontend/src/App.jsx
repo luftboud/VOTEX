7aeb749aca89e2ba595987e8f2f9591e181ea903
Ija Maharyta <ijamaharyta@gmail.com>
2026-03-29 14:57:26 +0300

    Add Login component and LoginPage structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
 
             {/*<Routes>*/}
             {/*    <Route path="/" element={<Home />} />*/}
+            {/*    <Route path="/login" element={<LoginPage />} />*/}
             {/*    <Route path="/user-instruction" element={<UserInstruction />} />*/}
             {/*</Routes>*/}
 
---
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Login from '../../components/Login/Login'
+import Footer from "../../components/Footer/Footer";
+
+function LoginPage() {
+    return (
+        <div>
+            <Login />
+            <Footer/>
+        </div>
+    )
+}
+export default LoginPage;
