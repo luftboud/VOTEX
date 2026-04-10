da17a924912911320007d819b2d359db3ddbc1b7
Ija Maharyta <ijamaharyta@gmail.com>
2026-04-10 20:04:46 +0300

    add files for admin homepage

---
+++ frontend/src/pages/AdminHome/AdminHome.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Header";
+
+function adminHome() {
+    return (
+        <div>
+            <Header
+                name="Ія"
+                avatar="/images/avatar.svg"
+            />
+            Hello, it's admin (like a boss)
+        </div>
+    )
+}
+
+export default adminHome;
